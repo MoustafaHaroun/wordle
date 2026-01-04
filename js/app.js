0b2c3f8048f2b6e3949683f815b8fb8edbac8795
Moustafa Haroun <Moustafa_Haroun@outlook.com>
2026-01-04 13:10:02 +0100

    init: add wordle

---
+++ js/app.js
@@ -0,0 +1,215 @@
+let wordOfTheDay = "";
+let guess = "";
+
+const maxGuessLenght = 5;
+const maxNumberOfGuesses = 5;
+
+let isLetterVal = false;
+
+let numberOfGuesses = 0;
+
+let gameDone = false;
+
+const wordsApiUrl = "https://words.dev-apis.com";
+
+let currentRow = document.querySelectorAll(".row")[numberOfGuesses];
+
+init();
+
+async function init() {
+    wordOfTheDay = await getWordOfTheDay();
+
+    document.addEventListener("keydown", function (event) {
+        isLetterVal = isLetter(event.key);
+
+        switch (event.key) {
+            case "Backspace":
+                handleBack();
+                break;
+            case "Enter":
+                handleSubmit();
+                break;
+            default:
+                if (isLetterVal & !gameDone) {
+                    handelInput(event.key);
+                }
+                break;
+        }
+
+        rerender();
+    });
+}
+
+function handelInput(input) {
+    if (guess.length >= maxGuessLenght) {
+        shakeRow();
+        return;
+    }
+
+    inputEffect();
+
+    guess = guess + input;
+}
+
+function handleBack() {
+    guess = guess.slice(0, -1);
+}
+
+async function handleSubmit() {
+    if (guess.length < maxGuessLenght) {
+        shakeRow();
+        return;
+    }
+
+    const validWord = await isValidWord(guess);
+
+    if (!validWord) {
+        shakeRow();
+        alert("not a valid word");
+
+        return;
+    }
+
+    const correct = checkAnswer(guess);
+
+    if (correct) {
+        jumpingInput();
+        gameDone = true;
+    }
+
+    let row = document.querySelectorAll(".row")[numberOfGuesses];
+    let inputs = row.querySelectorAll(".letter-input");
+
+    for (let i = 0; i < inputs.length; i++) {
+        let currentInput = inputs[i];
+
+        let animationDelay = i * 0.5;
+
+        currentInput.style.setProperty("--flip-delay", animationDelay + "s");
+
+        currentInput.classList.add("card-flip");
+
+        let currentLetter = guess[i];
+
+        currentInput.addEventListener(
+            "animationend",
+            () => {
+                currentInput.classList.remove("card-flip");
+
+                currentInput.style.color = "white";
+
+                if (wordOfTheDay[i] === currentLetter) {
+                    currentInput.style.backgroundColor = "var(--wordle-green)";
+                    currentInput.style.borderColor = "var(--wordle-green)";
+                } else if (wordOfTheDay.includes(currentLetter)) {
+                    currentInput.style.backgroundColor = "var(--wordle-yellow)";
+                    currentInput.style.borderColor = "var(--wordle-yellow)";
+                } else {
+                    currentInput.style.backgroundColor = "var(--wordle-gray)";
+                    currentInput.style.borderColor = "var(--wordle-gray)";
+                }
+            },
+            { once: true },
+        );
+    }
+
+    if (numberOfGuesses < 5) {
+        numberOfGuesses++;
+        guess = "";
+    }
+
+    if (numberOfGuesses == 6) {
+        gameDone = true;
+    }
+}
+
+function rerender() {
+    let row = document.querySelectorAll(".row")[numberOfGuesses];
+    let inputs = row.querySelectorAll(".letter-input");
+
+    for (let i = 0; i < inputs.length; i++) {
+        let letter = inputs[i].querySelector(".letter");
+        if (guess[i] === undefined) {
+            letter.innerText = "";
+        } else {
+            letter.innerText = guess[i];
+        }
+    }
+}
+
+function shakeRow() {
+    let row = document.querySelectorAll(".row")[numberOfGuesses];
+
+    row.classList.add("shake");
+
+    row.addEventListener(
+        "animationend",
+        () => {
+            row.classList.remove("shake");
+        },
+        { once: true },
+    );
+}
+
+function inputEffect() {
+    let row = document.querySelectorAll(".row")[numberOfGuesses];
+    let inputs = row.querySelectorAll(".letter-input");
+
+    let currentInput = inputs[guess.length];
+    currentInput.classList.add("input-effect");
+
+    currentInput.addEventListener(
+        "animationend",
+        () => {
+            currentInput.classList.remove("input-effect");
+        },
+        { once: true },
+    );
+}
+
+function jumpingInput() {
+    let row = document.querySelectorAll(".row")[numberOfGuesses];
+    let inputs = row.querySelectorAll(".letter-input");
+
+    for (let i = 0; i < inputs.length; i++) {
+        let currentInput = inputs[i];
+
+        currentInput.classList.add("jumping");
+
+        currentInput.addEventListener(
+            "animationend",
+            () => {
+                currentInput.classList.remove("jumping");
+            },
+            { once: true },
+        );
+    }
+}
+
+async function getWordOfTheDay() {
+    const promise = await fetch(wordsApiUrl + "/word-of-the-day");
+    const processedResponse = await promise.json();
+
+    console.log(processedResponse.word);
+
+    return processedResponse.word;
+}
+
+async function isValidWord(word) {
+    const promise = await fetch(wordsApiUrl + "/validate-word", {
+        method: "POST",
+        body: JSON.stringify({ word: guess }),
+    });
+
+    const processedResponse = await promise.json();
+
+    return processedResponse.validWord;
+}
+
+function checkAnswer(guess) {
+    return guess == wordOfTheDay;
+}
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
